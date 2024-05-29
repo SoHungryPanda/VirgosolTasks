94392b13b85bbf16abe9770d425afbd5c2574e0a
SoHungryPanda <saadetgaffaroglu@gmail.com>
2024-05-29 15:06:46 +0300

    first commit for Virgosol tasks

--- src/Odev9.java
+++ src/Odev9.java
@@ -4,19 +4,11 @@ public class Odev9 {
         int inttutar = (int) tutar;
         int ikisitoplami = inttutar + inttutar;
         //byte byteTutar = (byte) inttutar;
-        //Üstteki bu dönüşüm çalışmadı, neden çalışmadığını araştır.
-
-        //Veli'nin ya da Ali'nin aldığı ürünler ya da paraları miktarına dair bilgi verilmediği için
-        // ikisinin de verilen fiyattaki üründen birer tane aldığı
-        // ve parlarının toplamının da bunların toplamı olduğu varsayılmıştır.
         System.out.print("Tutar:");
         System.out.printf("%,.2f" , tutar);
-        System.out.printf("\n");
-        System.out.print("Ali'nin görmek istediği v1:");
+        System.out.print("\n"+"Ali'nin görmek istediği v1:");
         System.out.printf("%,.0f" , tutar);
-        //Hemen üstteki de tutarın print edilirken dönüştürülmesini sağlayan metot.
-        System.out.printf("\n");
-        System.out.print("Ali'nin görmek istediği v2:");
+        System.out.print("\n"+"Ali'nin görmek istediği v2:");
         System.out.println(inttutar);
         System.out.println("Veli'nin birikimi (tam sayı):" + inttutar);
         System.out.println("Veli'nin birikimi (byte):" + Integer.toBinaryString(inttutar));
